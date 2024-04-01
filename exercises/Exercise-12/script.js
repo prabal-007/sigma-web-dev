32952b41cec3addb44178b3293c6229e346d89e3
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-04-01 15:33:58 +0530

    erercise 12 - Random color Boxes

---
+++ exercises/Exercise-12/script.js
@@ -0,0 +1,18 @@
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i=0; i<6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// let boxes = document.getElementsByClassName('box');
+let boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    let color = getRandomColor();
+    let bg = getRandomColor();
+    box.style.color = color;
+    box.style.background = bg;
+});
