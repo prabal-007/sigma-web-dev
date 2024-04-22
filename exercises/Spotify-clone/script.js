da3c51dd440637432322b7a36956c982ec154fa2
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-04-23 00:26:26 +0530

    Project 2 - Spotify Clone

--- exercises/Spotify-clone/script.js
+++ exercises/Spotify-clone/script.js
@@ -1,14 +1,15 @@
 console.log('lets write Javascript now..');
+let currentSong = new Audio();
 
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:3000/exercises/Spotify-clone/songs/');
     let response = await a.text();
-    console.log(response);
+    // console.log(response);
     let div = document.createElement('div');
     div.innerHTML = response;
     let as = div.getElementsByTagName('a');
-    console.log('here');
-    console.log(as);
+    // console.log('here');
+    // console.log(as);
     let songs = [];
     for (let i = 0; i < as.length; i++) {
         const element = as[i];
@@ -19,11 +20,23 @@ async function getSongs() {
     return songs;
 }
 
-async function main() {
-    let songs = await getSongs();
-    console.log(songs);
+function playMusic(tarck) {
+    // var audio = new Audio('/exercises/Spotify-clone/songs/' + tarck);
+    currentSong.src = '/exercises/Spotify-clone/songs/' + tarck;
+    currentSong.play();
+}
 
-    let songUl = document.querySelector('.songlist').getElementsByTagName('ul')[0];
+// const playMusic = (tarck) => {
+//     let audio = new Audio("/exercises/Spotify-clone/songs/" + tarck);
+//     audio.play();
+// }
+
+async function main() {
+    
+    let songs = await getSongs();
+    // console.log(songs);
+
+    let songUl = document.querySelector('.songList').getElementsByTagName('ul')[0];
     //create li elements and add them to the ul
     
     for (const song of songs) {
@@ -38,13 +51,32 @@ async function main() {
                             <div class="playnow">
                                 <span>Play now</span>
                                 <img src="play.svg" alt="Play Button">
-                            </div>
-                            
-                        </li>`;
+                            </div> </li>`;
     }
 
-    var audio = new Audio(songs[10]);
-    audio.play();
+    // var audio = new Audio(songs[10]);
+    // audio.play();
+
+    Array.from(document.querySelector('.songList').getElementsByTagName('li')).forEach(e=>{
+        e.addEventListener('click', element=>{
+            play.src = 'pause.svg';
+            console.log(e.querySelector('.info').firstElementChild.innerHTML)
+            playMusic(e.querySelector('.info').firstElementChild.innerHTML.trim())
+            // e.querySelector('.playnow').getElementsByTagName('img')[0].src = 'pause.svg';
+        })
+    })
+
+    play.addEventListener('click', ()=> {
+        if (currentSong.paused){
+            currentSong.play();
+            play.src='pause.svg';
+        } else {
+            currentSong.pause();
+            play.src = 'play.svg';
+        }
+    })
+
+
 }
 
 main();
