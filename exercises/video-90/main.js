ea2e971b289d8f43c63341d9a7dfc526e069c034
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-06-13 23:47:24 +0530

    Video 90 - Middlewares in Express

--- exercises/video-90/main.js
+++ exercises/video-90/main.js
@@ -4,22 +4,25 @@ const fs = require('fs')
 const blog = require('./routes/blog')
 const port  = 3000
 
+// inbuilt middleware (express)
 app.use(express.static('public'))
+
+// routers middleware
 app.use('/blog', blog)
 
+// application-level middleware   /   custome middleware 1
 app.use((req, res, next) => {
     req.stark = 'Hi!, I am Stark'
-    fs.appendFileSync('log.txt', `A ${req.method} request was made at timestamp ${Date.now()}\n`)
+    fs.appendFileSync('log.txt', `A ${req.method} request was made at timestamp ${Date.now()}\n`)    // logging info
     console.log('M1')
     next()
 })
 
+// custome middleware 2
 app.use((req, res, next) => {
     console.log('M2')
     next()
 })
-
-
 
 app.get('/', (req, res) => {
     res.send('Hello There !')
--- exercises/video-90/routes/blog.js
+++ exercises/video-90/routes/blog.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
+// router-level  middlewares
 router.use((req, res, next) => {
     console.log('M3')
     fs.appendFileSync('log.txt', `Logged in blog at ${Date.now()}\n`)
     next()
 })
-
 
 router.get('/', (req, res) => {
     res.send('Blog Homepage')
